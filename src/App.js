f99e0cc6195645d14ecbce7a01169da7336c2dbe
Jojo Dong <JojoD@hynds.co.nz>
2021-08-12 09:30:54 +1200

    second commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+// import React, { Component } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function App () {
+    const [message, setMessage] = useState ("");
+    const api = 'https://1zwn6mu9jg.execute-api.ap-southeast-2.amazonaws.com/staging';
+    const data = { "name" : "Jojo" };
+
+    useEffect (() => {
+      axios
+      .post(api, data)
+      .then((response) => {
+        
+        setMessage(response.data);
+        console.log(response);
+        console.log(message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }, [])
+
+    return (
+      <div>{message}</div>
+    );
+
+}
+export default App;
